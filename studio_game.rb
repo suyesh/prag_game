713956c5ff831b5011e0cce218ea936c9b26791d
Suyesh Bhandari <suyeshb@gmail.com>
2015-07-15 17:23:12 -0400

    First Commit

---
+++ studio_game.rb
@@ -0,0 +1,9 @@
+player1 = "larry"
+health1 = 60
+player2 = "curly"
+player3 = "moe"
+
+puts "#{player1}'s health is #{health1/9.0}"
+puts "#{player1}'s health is #{health1/9}"
+
+puts "Players:\n\t#{player1}\n\t#{player2}\n\t#{player3}"
