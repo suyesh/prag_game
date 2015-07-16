ea91b09e82acbd20f76fcaae6ca5308f84a268e3
Suyesh Bhandari <suyeshb@gmail.com>
2015-07-15 20:10:06 -0400

    Variables and objects

--- studio_game.rb
+++ studio_game.rb
@@ -1,9 +1,25 @@
 player1 = "larry"
 health1 = 60
 player2 = "curly"
+health2 = 125
 player3 = "moe"
+health3 = 100
+player4 ="shemp"
+health4 = 90
 
-puts "#{player1}'s health is #{health1/9.0}"
-puts "#{player1}'s health is #{health1/9}"
 
-puts "Players:\n\t#{player1}\n\t#{player2}\n\t#{player3}"
+puts "#{player1.capitalize}'s health is #{health1}"
+puts "#{player2.upcase} has an health of #{health2}"
+
+health2 = health1
+
+puts "#{player2.upcase} has an health of #{health2}"
+
+health1 = 30
+
+puts "Larry: #{health1}"
+puts "Curly: #{health2}"
+
+puts "#{player3.capitalize} has a health of #{health3}.".center(50, '*')
+
+puts "#{player4.capitalize.ljust(30, '.')} #{health4} health"
