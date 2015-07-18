0bd6c932372dd06c5c37055351fb927da28e63b6
Suyesh Bhandari <suyeshb@gmail.com>
2015-07-18 07:27:06 -0400

    Classes

---
+++ calculator.rb
@@ -0,0 +1,18 @@
+class Calculator
+  def initialize(num1, num2)
+    @num1 = num1
+    @num2 = num2
+  end
+
+  def add
+    @num1 + @num2
+  end
+
+  def substract
+    @num1 - @num2
+  end
+end
+
+calc = Calculator.new(20,11)
+puts calc.add
+puts calc.substract
--- studio_game.rb
+++ studio_game.rb
@@ -1,23 +1,33 @@
+class Player
+	def initialize(name,health=100)
+		@name = name.capitalize
+		@health = health
+	end
 
-def say_hello(name, health=100)
-	current_time = Time.new
-	time = current_time.strftime("%I:%M:%S")
-	puts "I'm #{name.capitalize} and my health is #{health} as of #{time}"
+	def to_s
+		"I am #{@name} with health of #{@health}"
+	end
+
+	def blam
+		@health -= 10
+		puts "#{@name} got blammed"
+	end
+
+	def w00t
+		@health += 15
+		puts "#{@name} got w00ted"
+	end
 end
 
+player1 = Player.new("moe")
+player2 = Player.new("larry", 60)
+player3 = Player.new("curly", 125)
 
 
-player1 = "larry"
-health1 = 60
-player2 = "curly"
-health2 = 125
-player3 = "moe"
-health3 = 100
-player4 ="shemp"
-health4 = 90
-
-
-
-say_hello(player1, health1)
-say_hello(player2, health2)
-say_hello(player3)
+puts player1
+puts player2
+puts player3
+player3.blam
+puts player3
+player3.w00t
+puts player3
