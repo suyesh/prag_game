7aa81101a62fa0e50b4adaa5431816efc5b24671
Suyesh Bhandari <suyeshb@gmail.com>
2015-07-20 03:05:01 -0400

    little more tidy

--- studio_game.rb
+++ studio_game.rb
@@ -25,25 +25,35 @@ class Player
 	end
 end
 
+class Game
+	attr_reader :title
+	def initialize(title)
+		@title = title
+		@players = []
+	end
+
+	def add_player(a_player)
+		@players << a_player
+	end
+
+	def play
+		puts "There are #{@players.size} players in game #{@title}"
+		@players.each do |player|
+			puts player
+		end
+		@players.each do |player|
+			player.w00t
+			player.blam
+			puts player
+		end
+	end
+end
 player1 = Player.new("moe")
 player2 = Player.new("larry", 60)
 player3 = Player.new("curly", 125)
-player4 = Player.new("Shemp", 90)
 
-players = [player1, player2, player3]
-puts "There are #{players.size} players in the game."
-
-
-players.each do |player|
-	player.w00t
-	player.blam
-	player.w00t
-	puts player
-end
-
-players.pop
-players << player4
-
-players.each do |player|
-	puts player
-end
+knuckleheads = Game.new("knuckleheads")
+knuckleheads.add_player(player1)
+knuckleheads.add_player(player2)
+knuckleheads.add_player(player3)
+knuckleheads.play
