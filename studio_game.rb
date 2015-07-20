570cb1e5aca22c4247ae23a3f3b843ef4b8a1cfe
Suyesh Bhandari <suyeshb@gmail.com>
2015-07-19 22:27:58 -0400

    Arrays

--- studio_game.rb
+++ studio_game.rb
@@ -28,18 +28,22 @@ end
 player1 = Player.new("moe")
 player2 = Player.new("larry", 60)
 player3 = Player.new("curly", 125)
+player4 = Player.new("Shemp", 90)
+
+players = [player1, player2, player3]
+puts "There are #{players.size} players in the game."
 
 
-puts player1
-puts player2
-puts player3
-player3.blam
-puts player3
-player3.w00t
-puts player3
-puts player1.health
-puts player2.health
-puts player3.health
+players.each do |player|
+	player.w00t
+	player.blam
+	player.w00t
+	puts player
+end
 
-player2.name = "Lawrence"
-puts player2.name
+players.pop
+players << player4
+
+players.each do |player|
+	puts player
+end
